005b5b2363c0217d14ac9d6bd301e8a3d413575e
waypse <slavapankov20@gmail.com>
2022-02-09 11:57:25 +0100

    .

--- index.js
+++ index.js
@@ -29,6 +29,34 @@ let date = new Date();
 let dayOfWeekNumber = date.getDay();
 let leKick;
 let quote;
+let quotes;
+function rndquote() {
+    let rnd = Math.floor(Math.random() * 7);
+    switch (rnd) {
+        case 0:
+            quotes = "qui a appelé aymen ?";
+            break;
+        case 1:
+            quotes = "Aymen c'est une bonne grosse michto";
+            break;
+        case 2:
+            quotes = "AYMEN FTG AVEC TES PRIMO";
+            break;
+        case 3:
+            quotes = "LIAM ARRETE DE CHANTER";
+            break;
+        case 4:
+            quotes = 'Le pd du jour est tedjini!';
+            break;
+        case 5:
+            quotes = 'Pour le weekend, aymen arrete de sucer';
+            break;
+        case 6:
+            quotes = 'Allez aymen degage';
+            break;
+    }
+    return quotes;
+}
 switch (dayOfWeekNumber) {
     case 0:
         leKick = "@Vasco";
@@ -69,9 +97,14 @@ client.on('ready', () => {
     console.log('the bot is ready!');
 });
 client.on('messageCreate', (message) => {
-    if (message.content === '.pd') {
+    if (message.content === '.pddujour') {
         message.reply({
             content: quote,
+        });
+    }
+    if (message.content === ".pd") {
+        message.reply({
+            content: rndquote(),
         });
     }
 });
--- index.ts
+++ index.ts
@@ -6,6 +6,35 @@ let date = new Date();
 let dayOfWeekNumber = date.getDay();
 let leKick: string;
 let quote : string;
+let quotes : string;
+
+function rndquote () {
+    let rnd = Math.floor(Math.random() * 7);
+    switch(rnd){
+        case 0: 
+            quotes = "qui a appelé aymen ?";
+            break;
+        case 1:
+            quotes = "Aymen c'est une bonne grosse michto";
+            break;
+        case 2:
+            quotes = "AYMEN FTG AVEC TES PRIMO";
+            break;
+        case 3:
+            quotes = "LIAM ARRETE DE CHANTER";
+            break;
+        case 4:
+            quotes = 'Le pd du jour est tedjini!';
+            break;
+        case 5:
+            quotes = 'Pour le weekend, aymen arrete de sucer';
+            break;
+        case 6:
+            quotes = 'Allez aymen degage';
+            break;
+    }
+    return quotes;
+}
 
 switch(dayOfWeekNumber){
     case 0: 
@@ -51,11 +80,16 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
-    if( message.content === '.pd') {
+    if( message.content === '.pddujour') {
         message.reply({
             content: quote,
         })
-    }    
+    } 
+    if (message.content === ".pd") {
+        message.reply({
+            content: rndquote(),
+        })
+    }  
 })
 
 client.login(process.env.TOKEN);
