2588eb1ee065cb61f1655a60db017f414f9a6bbd
waypse <slavapankov20@gmail.com>
2022-02-15 10:06:08 +0100

    .

--- commands/pddujour.ts
+++ commands/pddujour.ts
@@ -42,6 +42,35 @@ export default {
     description: "replies",
 
     callback: ({ message }) => {
+        let date = new Date();
+        let dayOfWeekNumber = date.getDay();
+        let quote:string = ''
+        
+        switch(dayOfWeekNumber){
+            case 0:
+                quote = "c'est vasco le gros gay";
+                break;
+            case 1:
+                quote = "kymeto suce";
+                break;
+            case 2:
+                quote = "Liam, t'est pd";
+                break;
+            case 3:
+                quote = "Aymen est encore le plus gros pd";
+                break;
+            case 4:
+                quote = 'Le pd du jour est tedjini!';
+                break;
+            case 5:
+                quote = 'Pour le weekend, aymen arrete de sucer';
+                break;
+            case 6:
+                quote = 'Allez aymen degage';
+                break;
+        }
+
+
         message.reply({
             content: quote,
         })
